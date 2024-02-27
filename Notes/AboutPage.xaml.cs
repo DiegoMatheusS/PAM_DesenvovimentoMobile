6b30b6070c022d14ce0fc242c84bd84a0d8e8944
Diego Matheus <dms.diego.2019@gmail.com>
2024-02-26 22:48:49 -0300

    fest: Codigo adicionados

---
+++ Notes/AboutPage.xaml.cs
@@ -0,0 +1,17 @@
+namespace Notes;
+
+public partial class AboutPage : ContentPage
+{
+	string fileName = "";
+	public AboutPage()
+	{
+		InitializeComponent();
+
+
+	}
+		private void LearnMore_Clicked(object sender, EventArgs e)
+	{
+
+    }
+
+}
